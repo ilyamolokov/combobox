097e2e11f211a8b08485744b6dcc3f12aaf58984
Ilya Molokov <lumberjack.im@gmail.com>
2023-01-22 15:54:23 +0500

    initial commit

--- src/components/combo-box/combo-box.tsx
+++ src/components/combo-box/combo-box.tsx
@@ -1,3 +1,70 @@
-export function ComboBox() {
-  return null;
+import React, { useState } from 'react';
+import './combo-box.css';
+
+interface ComboBoxProps {
+  itemList: { label: string; year: number }[];
+  value: string;
+  onChange: (val: string) => void;
+}
+
+export function ComboBox(props: ComboBoxProps) {
+  const { itemList, value, onChange } = props;
+  const [focused, setFocused] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setFocused(false);
+    }, 250);
+  };
+
+  const handleClick = (value: string) => {
+    onChange(value);
+    setFocused(false);
+  };
+
+  const filteredItems = value
+    ? itemList.filter((item) =>
+        item.label.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+      )
+    : itemList;
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.keyCode === 13) {
+      onChange('');
+    }
+  };
+
+  return (
+    <div>
+      <ul className="dropdown">
+        <h3>Combo box</h3>
+        <input
+          id="search"
+          onFocus={() => setFocused(true)}
+          onBlur={handleBlur}
+          type="text"
+          placeholder="Search"
+          onChange={handleChange}
+          value={value}
+          onKeyDown={handleKeyPress}
+        />
+        {focused &&
+          filteredItems.map((item) => {
+            return (
+              <li
+                onClick={() => handleClick(item.label)}
+                className="dropdown-item"
+                key={item.label}
+              >
+                {item.label}
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
 }
